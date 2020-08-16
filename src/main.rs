ba210063bbe69e563cf2db06980dc9cecc8d929e
成林 <5231628+JackLinXX@user.noreply.gitee.com>
2020-08-16 20:43:45 +0800

    Finished

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
-use structopt::Struct;
+use structopt::StructOpt;
 
 
 
-
+#[derive(StructOpt, Debug)]
+struct Cli{
+    command: u32,
+    data: String,
+}
 
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::from_args();
+    match cli.command {
+        1 => {
+            println!("this is encrytion");
+        }
+        2 => {
+            println!("this is decrytion");
+        }
+        _ => {
+            println!("please input 1(encrytion)/2(decrytion)");
+        }
+    }
+    println!("{:#?}",cli);
 }
